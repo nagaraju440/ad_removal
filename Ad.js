36544fc39b16cc1bed4373f3b51b6417430b1e0c
nagaraju440 <70122331+nagaraju440@users.noreply.github.com>
2022-09-09 19:28:51 +0530

    Add files via upload

---
+++ Ad.js
@@ -0,0 +1,19 @@
+var c1=document.getElementById("c1")
+var c2=document.getElementById("c2")
+var c3=document.getElementById("c3")
+var c4=document.getElementById("c4")
+
+
+function closeC1(){ 
+        c1.style.display="none"
+        c2.style.display="block"
+}
+
+function closeC2(){
+         c2.style.display="none";
+         c3.style.display="block"
+}
+function closeC3(){
+        c3.style.display="none";
+        c4.style.display="block"
+}
